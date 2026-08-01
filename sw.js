c818ad70b977960361aed331154fbcc1fec30b8f
Eituprocoder <eitupro256@gmail.com>
2026-08-01 11:33:21 +0300

    Update cache name for service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 1. Cambia el número de versión cada vez que modifiques tu HTML u otros archivos
-const cacheName = 'Pac-Fight-RPG'; 
+const cacheName = 'Pac-Fight-RPG-Game'; 
 
 const assets = [
   './',
